0c20a8aac0ace895bf9e16f1d1f6d8bc08e61173
Alex Kamsteeg <alex@atlex.nl>
2019-05-15 18:26:02 +0200

    Fixed not being able to run benchmarks without a solution file (fixes #1, version 1.2.1)

--- src/AtleX.BenchmarkDotNet.Template.CSharp/content/Program.cs
+++ src/AtleX.BenchmarkDotNet.Template.CSharp/content/Program.cs
@@ -2,6 +2,9 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
 
 namespace AtleX.BenchmarkDotNet.Template.CSharp
 {
@@ -11,6 +14,10 @@ namespace AtleX.BenchmarkDotNet.Template.CSharp
         {
 			var config = ManualConfig
 				.Create(DefaultConfig.Instance);
+			config.Add(
+				Job.ShortRun.With(
+					InProcessEmitToolchain.Instance  // Run in-process so you don't need a solution file. If this is part of a solution, replace with CsProjCoreToolchain.NetCoreApp21.
+				)
 				
             var summary = BenchmarkRunner.Run<Benchmarks>(config);
         }
